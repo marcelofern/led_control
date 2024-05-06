8f790945011facc864b1dcd050ed280910023fb1
Marcelo Fernandes <marceelofernandes@gmail.com>
2024-05-06 19:00:42 +1200

    Remove unused #define macros

--- src/rainbow.c
+++ src/rainbow.c
@@ -6,17 +6,6 @@
 
 #define NUM_PERFORMANCES 6
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  ((byte) & 0x80 ? '1' : '0'), \
-  ((byte) & 0x40 ? '1' : '0'), \
-  ((byte) & 0x20 ? '1' : '0'), \
-  ((byte) & 0x10 ? '1' : '0'), \
-  ((byte) & 0x08 ? '1' : '0'), \
-  ((byte) & 0x04 ? '1' : '0'), \
-  ((byte) & 0x02 ? '1' : '0'), \
-  ((byte) & 0x01 ? '1' : '0')
-
 int8_t performance_idx = 0;
 
 void configure_rainbow() {
